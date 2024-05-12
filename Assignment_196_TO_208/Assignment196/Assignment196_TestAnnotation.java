7e4c003ab1ebe398424d5536cd51d585d79825de
Mishu-21 <chauhanhimanshi051@gmail.com>
2024-05-12 18:33:07 +0530

    Assignment 196

---
+++ Assignment_196_TO_208/Assignment196/Assignment196_TestAnnotation.java
@@ -0,0 +1,19 @@
+//Write a program for '@Test' Annotations in TestNG and check the report
+package Assignment;
+
+import org.testng.annotations.Test;
+
+public class Assignment196_TestAnnotation {
+
+	@Test
+	public void LOGIN()
+	{
+		System.out.println("LOGIN");
+	}
+	
+	@Test
+	public void login()
+	{
+		System.out.println("login");
+	}
+}
